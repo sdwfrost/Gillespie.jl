aca907819ee59af75e14c6fcd7ba2961c2d84c2b
Simon Frost <sdwfrost@gmail.com>
2016-07-22 22:16:02 +0100

    Fixing doc deployment

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,5 @@ makedocs(modules=[Gillespie],
 
 deploydocs(
     deps = Deps.pip("mkdocs","python-markdown-math"),
-    repo = "github.com/sdwfrost/Gillespie.jl.git",
-    julia = "0.4.6",
-    osname = "linux"
+    repo = "github.com/sdwfrost/Gillespie.jl.git"
 )
