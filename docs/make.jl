e8db00f5915075976c56ee21cc4d91709d246e48
Simon Frost <sdwfrost@gmail.com>
2016-07-22 22:02:37 +0100

    Trying to fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,11 @@
 using Documenter, Gillespie
 
-makedocs()
+makedocs(modules=[Gillespie],
+    doctest = false)
 
 deploydocs(
     deps = Deps.pip("mkdocs","python-markdown-math"),
     repo = "github.com/sdwfrost/Gillespie.jl.git",
-    julia = "0.4"
+    julia = "0.4.6",
+    osname = "linux"
 )
