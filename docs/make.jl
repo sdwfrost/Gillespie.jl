0acfc5fcbd63de88f3c2afe847c143469a48080b
Simon Frost <sdwfrost@gmail.com>
2016-07-22 21:05:15 +0100

    More Documenter tweaks

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,9 @@
 using Documenter, Gillespie
 
 makedocs()
+
+deploydocs(
+    deps = Deps.pip("mkdocs","python-markdown-math"),
+    repo = "github.com/sdwfrost/Gillespie.jl.git",
+    julia = "0.4"
+)
